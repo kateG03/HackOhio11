6fcde6d18cca9190408b1f0d131581e147aad740
Ardargoz Aktilan <aktilanrp@gmail.com>
2023-10-28 17:29:47 -0400

    Continued working on MessyTree getPath()

--- lib/logic/messy_tree.dart
+++ lib/logic/messy_tree.dart
@@ -13,20 +13,28 @@ class MessyNode {
   void addConnection(MessyNode node) {
     connections.insert(0, node);
   }
+
+  bool isEqual(MessyNode node){
+    return node.latitude==latitude && node.longitude==latitude;
+  }
 }
 
 class MessyTree {
   MessyNode head = MessyNode(latitude: 0, longitude: 0);
 
-  List<MessyNode> getPath(MessyNode start, MessyNode end) {
+  List<MessyNode> getPaths(MessyNode start, MessyNode end, MessyNode previous) {
     List<MessyNode> path = List.empty(growable: true);
     
     if (start.isConnected(end)) {
       path = [start, end];
     } else {
       for (MessyNode n in start.connections) {
-        path = getPath(n, end);
-        path.insert(0, start);
+        if(!n.isEqual(previous)){
+          path = getPaths(n, end, start);
+          if(path.isNotEmpty){
+            path.insert(0, start);
+          }
+        }
       }
     }
     return path;
