aaa2854bff59ac9fd589403124f9603b2c5f9f2d
Skylar Stephens <myloslayer1245@gmail.com>
2023-10-28 12:42:08 -0400

    fix

--- lib/UI/start_screen.dart
+++ lib/UI/start_screen.dart
@@ -1,7 +1,6 @@
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
-import 'package:easy_search_bar/easy_search_bar.dart';
 
 class StartScreen extends StatefulWidget {
   final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
