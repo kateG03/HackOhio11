14a849f3c8d04f0e0bd1f589185cf230446ea7d7
Ardargoz Aktilan <aktilanrp@gmail.com>
2023-10-28 15:12:52 -0400

    Added some location data

--- lib/logic.dart
+++ lib/logic.dart
@@ -15,14 +15,26 @@ double latitude;
 
 RoomNode Sloopy = RoomNode(
   name: "Sloopy's Diner", 
-  longitude: 39.997466, 
-  latitude: -83.008996
+  latitude: 39.997466, 
+  longitude: -83.008996
 );
 
 RoomNode Archie = RoomNode(
   name: "Archie M. Griffin Grand Ballroom", 
-  longitude: 39.997853, 
-  latitude: -83.008483
+  latitude: 39.997853, 
+  longitude: -83.008483
+);
+
+RoomNode Entrance = RoomNode(
+  name: "Ohio Union Entrance", 
+  latitude: 39.997593, 
+  longitude: -83.008965
+);
+
+RoomNode UnionMarket = RoomNode(
+  name: "Union Market", 
+  latitude: 39.997715, 
+  longitude: -83.008872
 );
 
 
