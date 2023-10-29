23133d406726fb20a79b07a4d02a9aa3cf77d5d9
Haley Bonidie <bonidie.2@buckeyemail.osu.edu>
2023-10-28 22:08:35 -0400

    formatting

--- lib/logic/room.dart
+++ lib/logic/room.dart
@@ -17,211 +17,211 @@ class RoomList {
   void main()
   {
 
-  List<Room> roomList = [];
+    List<Room> roomList = [];
   //Basement
-  Room cowRoom = Room(
+    Room cowRoom = Room(
       name: "Maudine Cow Room",
       //FIXME
       nodes: [MessyNode(latitude: 39.997715, longitude: -83.008872)]);
       roomList.add(cowRoom);
 
-  Room creativeRoom = Room(
+    Room creativeRoom = Room(
       name: "Creative Arts Room",
       //FIXME
       nodes: [MessyNode(latitude: 39.997715, longitude: -83.008872)]);
 
-  Room mileRoom = Room(
+    Room mileRoom = Room(
       name: "Milestones Room",
       //FIXME
       nodes: [MessyNode(latitude: 39.997715, longitude: -83.008872)]);
 
-  Room danceRoom1 = Room(
+    Room danceRoom1 = Room(
       name: "Dance Room 1",
       //FIXME
       nodes: [MessyNode(latitude: 39.997715, longitude: -83.008872)]);
 
-  Room danceRoom2 = Room(
+    Room danceRoom2 = Room(
       name: "Dance Room 2",
       //FIXME
       nodes: [MessyNode(latitude: 39.997715, longitude: -83.008872)]);
 
-  Room kitchenRoom = Room(
+    Room kitchenRoom = Room(
       name: "Instructional Kitchen",
       //FIXME
       nodes: [MessyNode(latitude: 39.997715, longitude: -83.008872)]);
 
-  Room lowerLoungeRoom = Room(
+    Room lowerLoungeRoom = Room(
       name: "Lower Level Lounge",
       //FIXME
       nodes: [MessyNode(latitude: 39.997715, longitude: -83.008872)]);
       //------------------------------------------------------------------------
   //First floor
-Room entrance = Room(
+    Room entrance = Room(
     name: "Ohio Union Entrance",
     nodes: [MessyNode(latitude: 39.997593, longitude: -83.008965)]);
 
-  Room safetyRoom = Room(
+    Room safetyRoom = Room(
       name: "OSU Public Safety",
       //FIXME
       nodes: [MessyNode(latitude: 39.997715, longitude: -83.008872)]);
-  Room bookRoom = Room(
+    Room bookRoom = Room(
       name: "The Ohio State University Bookstore",
       //FIXME
       nodes: [MessyNode(latitude: 39.997715, longitude: -83.008872)]);
-  Room alsonsoRoom = Room(
+    Room alsonsoRoom = Room(
       name: "Alonso Family Room",
       //FIXME
       nodes: [MessyNode(latitude: 39.997715, longitude: -83.008872)]);
-   Room meetingRoom = Room(
+    Room meetingRoom = Room(
       name: "Great Hall Meeting Room",
       //FIXME
       nodes: [MessyNode(latitude: 39.997715, longitude: -83.008872)]);
       
-  Room infoCenter = Room(
+    Room infoCenter = Room(
       name: "Information Center",
       //FIXME
       nodes: [MessyNode(latitude: 39.997715, longitude: -83.008872)]);
-  Room alumniAssociation = Room(
+    Room alumniAssociation = Room(
       name: "The Ohio State Unversity Alumni Association",
       //FIXME
       nodes: [MessyNode(latitude: 39.997715, longitude: -83.008872)]);
-  Room woodyRoom = Room(
+    Room woodyRoom = Room(
       name: "Woody's Tavern",
       //FIXME
       nodes: [MessyNode(latitude: 39.997715, longitude: -83.008872)]);
-  Room performanceRoom = Room(
+    Room performanceRoom = Room(
       name: "Performance Hall",
       //FIXME
       nodes: [MessyNode(latitude: 39.997715, longitude: -83.008872)]);
-  Room unionMarket = Room(
+    Room unionMarket = Room(
       name: "Union Market",
       nodes: [MessyNode(latitude: 39.997715, longitude: -83.008872)]);
-  Room benLounge = Room(
+    Room benLounge = Room(
       name: "Ben & Arlene Roth Lounge",
       //FIXME
       nodes: [MessyNode(latitude: 39.997715, longitude: -83.008872)]);
-  Room bankTheater = Room(
+    Room bankTheater = Room(
       name: "US Bank Conference Theater",
       //FIXME
       nodes: [MessyNode(latitude: 39.997715, longitude: -83.008872)]);
-  Room espressRoom = Room(
+    Room espressRoom = Room(
       name: "Espress-OH",
       //FIXME
       nodes: [MessyNode(latitude: 39.997715, longitude: -83.008872)]);
-  Room sloopy = Room(
+    Room sloopy = Room(
     name: "Sloopy's Diner",
     nodes: [MessyNode(latitude: 39.997466, longitude: -83.008996)]);
 //------------------------------------------------------------------------
 //Second floor
-Room archie = Room(
+  Room archie = Room(
     name: "Archie M. Griffin Grand Ballroom",
     number: "2133",
     nodes: [MessyNode(latitude: 39.997853, longitude: -83.008483)]);
-Room dannyLounge = Room(
+  Room dannyLounge = Room(
     name: "Danny Price Student Lounge",
     //FIXME
     nodes: [MessyNode(latitude: 39.997593, longitude: -83.008965)]);
-Room adminRoom = Room(
+  Room adminRoom = Room(
     name: "Administrative Office Suite",
     //FIXME
     nodes: [MessyNode(latitude: 39.997593, longitude: -83.008965)]);
-Room senateRoom = Room(
+  Room senateRoom = Room(
     name: "Senate Chamber",
     //FIXME
     nodes: [MessyNode(latitude: 39.997593, longitude: -83.008965)]);
-Room traditionsRoom = Room(
+  Room traditionsRoom = Room(
     name: "Ohio Staters, INC. Traditions Room",
     //FIXME
     nodes: [MessyNode(latitude: 39.997593, longitude: -83.008965)]);
-Room glassLounge = Room(
+  Room glassLounge = Room(
     name: "Glass Art Lounge",
     //FIXME
     nodes: [MessyNode(latitude: 39.997593, longitude: -83.008965)]);
-Room keithRoom = Room(
+  Room keithRoom = Room(
     name: "Keith B. Key Center For Student Leadership & Service",
     //FIXME
     nodes: [MessyNode(latitude: 39.997593, longitude: -83.008965)]);
-Room studentAlumniRoom = Room(
+  Room studentAlumniRoom = Room(
     name: "Student-Alumni Council Room",
     //FIXME
     nodes: [MessyNode(latitude: 39.997593, longitude: -83.008965)]);
-Room sphinxRoom = Room(
+  Room sphinxRoom = Room(
     name: "Sphix Centennial Leadership Suite",
     //FIXME
     nodes: [MessyNode(latitude: 39.997593, longitude: -83.008965)]);
-Room foundersRoom = Room(
+  Room foundersRoom = Room(
     name: "Ohio Staters, INC. Founders Room",
     //FIXME
     nodes: [MessyNode(latitude: 39.997593, longitude: -83.008965)]);
 //------------------------------------------------------------------------
 //Third floor
 
-Room sigmaRoom = Room(
+  Room sigmaRoom = Room(
     name: "Sigma Phi Epsilon Lounge",
     //FIXME
     nodes: [MessyNode(latitude: 39.997593, longitude: -83.008965)]);
-Room undergraduateRoom = Room(
+    Room undergraduateRoom = Room(
     name: "Undergraduate Admission Welcome Center",
     //FIXME
     nodes: [MessyNode(latitude: 39.997593, longitude: -83.008965)]);
-Room interfaithRoom = Room(
+  Room interfaithRoom = Room(
     name: "Interfaith Prayer & Reflection Room",
     //FIXME
     nodes: [MessyNode(latitude: 39.997593, longitude: -83.008965)]);
-Room stanlenyRoom = Room(
+  Room stanlenyRoom = Room(
     name: "Stanley D. Gottesegen Study Lounge",
     //FIXME
     nodes: [MessyNode(latitude: 39.997593, longitude: -83.008965)]);
-Room officeRoom = Room(
+  Room officeRoom = Room(
     name: "Office of the Senior Vice President for Student Life",
     //FIXME
     nodes: [MessyNode(latitude: 39.997593, longitude: -83.008965)]);
-Room willieRoom = Room(
+  Room willieRoom = Room(
     name: "Willie J. Younge, SR. Off-Campus & Commuter Student Engagement",
     //FIXME
     nodes: [MessyNode(latitude: 39.997593, longitude: -83.008965)]);
-Room buckIDRoom = Room(
+  Room buckIDRoom = Room(
     name: "BUCKID",
     //FIXME
     nodes: [MessyNode(latitude: 39.997593, longitude: -83.008965)]);
-Room brutusRoom = Room(
+  Room brutusRoom = Room(
     name: "Brutus Buckeye Room",
     //FIXME
     nodes: [MessyNode(latitude: 39.997593, longitude: -83.008965)]);
-Room cartoonRoom = Room(
+  Room cartoonRoom = Room(
     name: "Cartoon Room",
     //FIXME
     nodes: [MessyNode(latitude: 39.997593, longitude: -83.008965)]);
-Room roundRoom = Room(
+  Room roundRoom = Room(
     name: "Round Meeting Room",
     //FIXME
     nodes: [MessyNode(latitude: 39.997593, longitude: -83.008965)]);
-Room keithKeyRoom = Room(
+  Room keithKeyRoom = Room(
     name: "Keith B. Key Center for Student Leadership & Service",
     //FIXME
     nodes: [MessyNode(latitude: 39.997593, longitude: -83.008965)]);
-Room barbieRoom = Room(
+  Room barbieRoom = Room(
     name: "Barbie Tootle Room",
     //FIXME
     nodes: [MessyNode(latitude: 39.997593, longitude: -83.008965)]);
-Room haysRoom = Room(
+  Room haysRoom = Room(
     name: "Hays Cape Room",
     //FIXME
     nodes: [MessyNode(latitude: 39.997593, longitude: -83.008965)]);
-Room tanyaRoom = Room(
+  Room tanyaRoom = Room(
     name: "Tanya Rutner Hartman Room",
     //FIXME
     nodes: [MessyNode(latitude: 39.997593, longitude: -83.008965)]);
-Room rosaRoom = Room(
+  Room rosaRoom = Room(
     name: "Rosa M. Ailabouni",
     //FIXME
     nodes: [MessyNode(latitude: 39.997593, longitude: -83.008965)]);
-Room suzanneRoom = Room(
+  Room suzanneRoom = Room(
     name: "Suzanne M. Scharer Room",
     //FIXME
     nodes: [MessyNode(latitude: 39.997593, longitude: -83.008965)]);
 
-}
+  }
 }
 //var rooms = new Map();
 //rooms[1050]="Performance Hall";
